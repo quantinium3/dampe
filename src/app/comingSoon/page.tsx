f4f771b56e46c1771f6b32611304db41aff2585c
HimanshuSolo2005 <himanshu.pv.verma@gmail.com>
2025-10-30 03:00:54 +0530

    coming-soon-page

---
+++ src/app/comingSoon/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TextHoverEffect } from '@/components/ui/text-hover-effect'
+
+export default function ComingSoonPage() {
+  return (
+    <div className="min-h-screen bg-gray-900">
+        <TextHoverEffect text="Coming Soon" />
+    </div>
+  )
+}
